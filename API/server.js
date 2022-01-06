f227406fa8457abbe4ad8c0f1f7b9f2e0cc35302
Daniel Pérez <daniel.prz.c@gmail.com>
2022-01-06 00:43:39 -0300

    feat: create apis

--- API/server.js
+++ API/server.js
@@ -1,4 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
-app.listen(4000, () => console.log("Server is up and running"));
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () =>
+  console.log(`Server is up and running on port ${port}...`)
+);
